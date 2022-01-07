d615f5eb5cfa4d46caf9fe47075bf7302b33d8d7
Christyasto Priyonggo Pambudi <christyasto.chito@gmail.com>
2022-01-07 19:36:27 +0700

    file naming
    extension name
    n zigzag brief

--- ZigZagSequence.java
+++ ZigZagSequence.java
@@ -1,7 +1,7 @@
 /*
-*   Given an array of  distinct integers, transform the array into a zig zag sequence by permuting the array elements. 
-*   A sequence will be called a zig zag sequence if the first  elements in the sequence are in increasing order and 
-*   the last  elements are in decreasing order, where . You need to find the lexicographically smallest zig zag sequence 
+*   Given an array of n distinct integers, transform the array into a zig zag sequence by permuting the array elements. 
+*   A sequence will be called a zig zag sequence if the first k elements in the sequence are in increasing order and 
+*   the last k elements are in decreasing order, where k = (n+1)/2. You need to find the lexicographically smallest zig zag sequence 
 *   of the given array.
 */
 import java.util.*;
